a49e0874713f536edeb9030732230444d4f9063a
“denis” <denis-kustov@rambler.ru>
2021-09-09 21:48:23 +0300

    add 4

--- __init__.py
+++ __init__.py
@@ -1,3 +1,9 @@
+from sys import  path
+
+from .template_obj import add_sys_path_if_not, rel_path
+
+add_sys_path_if_not(rel_path(0), path)
+
 from logic.debugger import *
 
 if __name__ == '__main__':
